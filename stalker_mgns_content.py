ffb4ce273b96e509b74aefa9de23a3313f3e0754
Karamazov-Ivan <oleg13.troshin@gmail.com>
2025-02-25 23:18:53 +0300

    0.00001

---
+++ stalker_mgns_content.py
@@ -0,0 +1,19 @@
+'''[package_content]
+porn3               = decor_poster3_1
+
+
+[package_content_uncommon]
+porn3               = decor_poster3_2
+
+
+[package_content_rare]
+porn3               = decor_poster3_3
+
+
+[package_content_epic]
+porn3               = decor_poster3_4
+
+
+[package_content_legendary]
+porn3               = decor_poster3_5
+'''
---
+++ test.py
@@ -0,0 +1,6 @@
+def some():
+    t = 1+2
+    b = 5+5
+    return t, b
+
+print(some()[0])
